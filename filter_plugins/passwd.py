026192d8eacf0914adca51accc7242476cefb6de
Rutger Rademaker <rutger.rademaker@gmail.com>
2024-05-23 20:12:13 +0200

    Learn some python

--- filter_plugins/passwd.py
+++ filter_plugins/passwd.py
@@ -1,25 +1,34 @@
-# via https://shantanoo-desai.github.io/posts/technology/mosquitto_ansible_passgen/
+"""Script to create a password file for Mosquitto with Ansible."""
 
+# Source: https://shantanoo-desai.github.io/posts/technology/mosquitto_ansible_passgen/
+
+import dataclasses
 from ansible.errors import AnsibleError
+try:
+    import passlib.hash
+except Exception as e:
+    raise AnsibleError('this script requires the passlib pip package installed') from e
 
 def mosquitto_passwd(passwd):
-    try:
-        import passlib.hash
-    except Exception as e:
-        raise AnsibleError('mosquitto_passlib custom filter requires the passlib pip package installed')
-    
-    SALT_SIZE = 12
-    ITERATIONS = 101
+    """Encrypt a mosquitto password."""
 
-    digest = passlib.hash.pbkdf2_sha512.using(salt_size=SALT_SIZE, rounds=ITERATIONS) \
-                                        .hash(passwd) \
-                                        .replace("pbkdf2-sha512", "7") \
-                                        .replace(".", "+")
-    
+    salt_size_value = 12
+    iterations_value = 101
+
+    digest = passlib.hash.pbkdf2_sha512.using(salt_size=salt_size_value, rounds=iterations_value) \
+        .hash(passwd) \
+        .replace("pbkdf2-sha512", "7") \
+        .replace(".", "+")
+
     return digest + "=="
 
-class FilterModule(object):
-    def filters(self):
+@dataclasses.dataclass
+class FilterModule():
+    """Filter Module"""
+
+    @staticmethod
+    def filters():
+        """List of filters"""
         return {
             'mosquitto_passwd': mosquitto_passwd,
         }
